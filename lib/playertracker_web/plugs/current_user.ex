91eba80eaab1e93560da71a2ff8c748f0b379a3c
Alex Tuley <alex.p.tuley@gmail.com>
2019-10-12 13:40:57 -0400

    Add init to plug

--- lib/playertracker_web/plugs/current_user.ex
+++ lib/playertracker_web/plugs/current_user.ex
@@ -1,6 +1,8 @@
 defmodule PlayertrackerWeb.Plugs.CurrentUser do
   import Plug.Conn
 
+  def init(default), do: default
+
   def call(conn, _opts) do
     conn
     |> assign(:current_user, Guardian.Plug.current_resource(conn))  
